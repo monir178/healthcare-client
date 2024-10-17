51ff803c431d4ed5d8078ce04af484e3c42e471e
Moniruzzaman Monir <monir.mzs17@gmail.com>
2024-10-17 20:59:42 +0600

    Navbar Footer Added

---
+++ src/components/Shared/Footer/Footer.tsx
@@ -0,0 +1,59 @@
+import { Box, Container, Stack, Typography } from "@mui/material";
+import Image from "next/image";
+import Link from "next/link";
+import facebookIcon from "@/assets/landing_page/facebook.png";
+
+const Footer = () => {
+  return (
+    <Box py={5} bgcolor="rgb(17, 26, 34)">
+      <Container>
+        <Stack direction="row" justifyContent="center" gap={4}>
+          <Typography color="#ffff" component={Link} href="/consultation">
+            Consultation
+          </Typography>
+          <Typography color="#ffff" component={Link} href="/login">
+            Health Plans
+          </Typography>
+          <Typography color="#ffff" component={Link} href="/login">
+            Medicine
+          </Typography>
+          <Typography color="#ffff" component={Link} href="/login">
+            Diagnostics
+          </Typography>
+          <Typography color="#ffff" component={Link} href="/login">
+            NGOs
+          </Typography>
+        </Stack>
+        <Stack direction="row" justifyContent="center" py={2} gap={2}>
+          <Image width={30} height={30} src={facebookIcon} alt="Faceboook" />
+          <Image width={30} height={30} src={facebookIcon} alt="Faceboook" />
+          <Image width={30} height={30} src={facebookIcon} alt="Faceboook" />
+          <Image width={30} height={30} src={facebookIcon} alt="Faceboook" />
+        </Stack>
+        <div className="border-b-[1px] border-dashed"></div>
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="space-between"
+          py={2}
+          gap={2}>
+          <Typography component="p" color="white">
+            &copy;2025 HealthCare. All Rights Reserved.
+          </Typography>
+          <Typography variant="h4" color="white">
+            <Box component="span" color="primary.main">
+              Health
+            </Box>{" "}
+            {""}
+            Care
+          </Typography>
+          <Typography component="p" color="white">
+            Privacy Policy! Terms & Conditions
+          </Typography>
+        </Stack>
+      </Container>
+    </Box>
+  );
+};
+
+export default Footer;
--- src/lib/theme/theme.ts
+++ src/lib/theme/theme.ts
@@ -23,13 +23,23 @@ export const theme = createTheme({
             styleOverrides: {
                 root: {
                     padding: '8px 24px',
+                    borderRadius: '8px',
+                    color: '#FFFFFF',
                 },
             },
         },
         MuiContainer: {
             defaultProps: {
-                maxWidth: 'lg',
+                maxWidth: 'xl',
             }
         }
+    },
+
+    typography: {
+        body1: {
+            color: '#0B1134CC',
+        },
     }
 });
+
+theme.shadows[1] = "0px 5px 22px lightgray";
